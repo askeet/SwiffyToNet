4656f737a7ce959c3b1fbeb62553ce0e6ab5b28a
askeet <cognizing@qip.ru>
2016-04-12 12:01:18 +0300

    Added files via upload

---
+++ Flash/SwiffyCode.js
@@ -0,0 +1,42 @@
+
+	var stage = new swiffy.Stage(document.getElementById('swiffycontainer'),
+          swiffyobject, {});   
+    		  
+    // Default. Set data from URL. Example Set Text1 = Text 1 and Text1 = 2 -  ?Text1=Text+1&Text2=2
+	/*var myQueryString=document.location.search;
+	if (myQueryString[0]='?')
+	{
+		myQueryString=myQueryString.substr(1, myQueryString.length-1);
+		//SetVariable("'"+myQueryString+"&&'");
+	}*/    
+    
+	stage.start();	  
+
+    // Action for JS Button	
+    var i = 0;	  
+    var reply_click = function()
+    {
+	   i=i+1;
+	   SetVariable("VarText1=6");
+	   //SetVariable("StrMovie=MovieClip1&GotoAndStop=2");
+	   SetGotoAndStop("MovieClip1","2");			
+	   //SetVariable("StrMovie=Text1&StrField=text&SetVar=100");
+	   SetField("Text1","text", i);	
+    }
+    document.getElementById("textChanger").onclick = reply_click;
+	
+	// Function
+
+    var SetVariable = function(Variable ){
+       stage.setFlashVars(Variable);	
+    }
+
+    var SetField = function(NameMovie,NameField, Variable ){
+       stage.setFlashVars("CurMovie="+NameMovie+"&CurField="+NameField+"&SetVar="+Variable);	
+    }
+
+    var SetGotoAndStop = function(NameMovie,NumberFrame ){
+      stage.setFlashVars("CurMovie="+NameMovie+"&GotoAndStop="+NumberFrame+"");	
+    }
+	
+	
