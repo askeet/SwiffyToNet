7d1cee04b956f008d2df8c48322947c891b1e95d
askeet <cognizing@qip.ru>
2016-04-12 20:17:54 +0300

    First Project

---
+++ NetProject/SwiftyProject/SwiftyProject/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SwiftyProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File HTML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(218, 23);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(369, 274);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "HTML files|*.html";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set Var";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 309);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ NetProject/SwiftyProject/SwiftyProject/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SwiftyProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                webBrowser1.Navigate(openFileDialog.FileName);
+                //webBrowser1.Document.InvokeScript("SetVariable", new Object[] { "VarText1=6" });
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("SetVariable", new Object[] { "VarText1=6" });
+        }
+    }
+}
